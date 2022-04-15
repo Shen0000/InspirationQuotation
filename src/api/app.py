cb88e9eb4f2bba1e891796513223a9c1d59f5f01
Shen0000 <73674912+Shen0000@users.noreply.github.com>
2022-04-15 11:51:37 -0400

    finish flask api and create Quotes temporary fetch page for testing

--- src/api/app.py
+++ src/api/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 from quotes import *
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello():
@@ -10,18 +12,29 @@ def hello():
 
 @app.route('/get-quotes', methods=['GET'])
 def getQuotes():
-    search = request.args.getlist('query')
+  
+    topics = request.args.getlist('query')
+    authors = request.args.getlist('author')
     # print(type(search))
     # print(search)
-    data = get_quotes(search)
+    data = get_quotes(topics)
+    data2 = get_quotes_by_author(authors)
     quotes = []
     authors = []
+
     for quote in data:
         # print(quote.split('\n'))
         split_text = quote.split('\n')
         quotes.append(split_text[0])
         authors.append(split_text[-1])
-    return json.dumps([quotes, authors])
+
+    for quote in data2:
+      # print(quote.split('\n'))
+      split_text = quote.split('\n')
+      quotes.append(split_text[0])
+      authors.append(split_text[-1])
+    
+    return jsonify([quotes, authors, len(quotes)]), 200
 
 if __name__ == "__main__":
   app.run()
