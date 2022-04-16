f9fcae9a1a760322a4a2f7dd118a9cd80d7f871f
Jayanth-Mani <69757920+Jayanth-Mani@users.noreply.github.com>
2022-04-16 19:40:00 -0400

    Add first navbar (image not responding right)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div>
-    <div class="navbar">
-      <ul>
-        <li v-if="!$store.state.loggedIn">
-          <router-link to="/login">Login</router-link>
-        </li>
-        <li v-if="$store.state.loggedIn">
-          <router-link to="/daily">View Daily Quote</router-link>
-        </li>
-        <li>
-          {{ this.$store.state.username }}
-        </li>
-        <li v-if="$store.state.loggedIn">
-          <button @click="logout">Logout</button>
-        </li>
-        <li><router-link to="/quotes">Quotes</router-link></li>
+    <ul class="navbar">
+      <li>
+        <router-link to="/">
+          <img
+            src="./assets/iq_logo.png"
+            alt="logo"
+            width="60"
+            height="60"
+            class="logo"
+          />
+        </router-link>
+      </li>
+      <li v-if="!$store.state.loggedIn">
+        <router-link to="/login" class="login-button">Login</router-link>
+      </li>
+      <li v-if="$store.state.loggedIn">
+        <router-link to="/daily" class="nav-element">Daily Quote</router-link>
+      </li>
+      <li v-if="$store.state.loggedIn">
+        <button @click="logout" class="nav-element">Logout</button>
+      </li>
+      <!-- <li>
+        <router-link to="/quotes" class="nav-element">Quotes</router-link>
+      </li> -->
+      <li v-if="$store.state.loggedIn">
+        <router-link to="/settings" class="nav-element">Settings</router-link>
+      </li>
+      <li>
+        <p>{{ this.$store.state.username }}</p>
+      </li>
+    </ul>
 
-        <li v-if="$store.state.loggedIn">
-          <router-link to="/settings">Settings</router-link>
-        </li>
-      </ul>
-      <router-link to="/quotes">Quotes</router-link>
+    <br />
+
+    <div class="main-container">
+      <router-view />
     </div>
-    <router-view />
   </div>
 </template>
 
@@ -64,7 +78,8 @@ export default {
 
 <style>
 body {
-  background-color: #64DFDF;
+  background-color: white;
+  margin: 0;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -72,18 +87,62 @@ body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
 ul {
   list-style-type: none;
   margin: 0;
   padding: 0;
+  overflow: hidden;
+  background-color: #333;
+  width: 100%;
 }
 
-.navbar {
-  border-style: solid;
+li {
+  color: white;
+  font-size: 14px;
+  height: 100%;
 }
-.nav-link {
-    text-decoration: none;
+
+li a {
+  float: left;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  height: 100%;
+}
+li button {
+  float: right;
+  background-color: #333;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  border: none;
+  cursor: pointer;
+  color: white;
+  font-size: 14px;
+}
+li p {
+  float: right;
+  vertical-align: text-top;
+  margin-right: 20px;
+}
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+  background-color: #111;
+}
+li button:hover {
+  background-color: #111;
+}
+img {
+  vertical-align: top;
+}
+.main-container {
+  margin-top: 100px;
+}
+.login-button {
+  float: right;
+  margin-right: 20px;
 }
 </style>
